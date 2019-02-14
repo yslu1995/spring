bdfde6bfaa75cc06b2cd66384e5b99a3bc584008
yslu <469899431@qq.com>
2019-02-14 13:49:28 +0800

    类

---
+++ src/com/how2java/interfaceTest/ADHero.java
@@ -0,0 +1,13 @@
+package com.how2java.interfaceTest;
+
+/**
+ * @Description: adc
+ * @Author: LYS
+ * @Date: 2019/2/13 16:52
+ */
+public class ADHero implements AD {
+    @Override
+    public void physicAttack() {
+        System.out.println("进行物理攻击");
+    }
+}
