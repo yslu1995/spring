c29c1bda5cf8382031f827ddbe03f9ab2ba0a11b
LYS <yslu@joinhealth.cn>
2019-02-25 20:01:32 +0800

    spring结束

---
+++ src/com/how2java/zhuangxiang/train/mahin.java
@@ -0,0 +1,36 @@
+package com.how2java.zhuangxiang.train;
+
+/**
+ * Created by LuYaoSheng
+ * Created time 2019/02/19 22:13
+ * Description
+ */
+public class mahin {
+    public static void main(String[] args) {
+
+        //int的最大值
+        System.out.println(Integer.MAX_VALUE);
+        //int的最小值
+        System.out.println(Integer.MIN_VALUE);
+
+//        1. 对byte,short,float,double进行自动拆箱和自动装箱
+        byte a = 1;
+        Byte A = a;
+        short b = 1;
+        Short B = b;
+        float c = 1l;
+        Float C = c;
+        double d = 3;
+        Double D = d;
+        System.out.println(A + "," + B + "," + C + "," + D);
+//        2. byte和Integer之间能否进行自动拆箱和自动装箱  不能
+        byte hh =12;
+//        Integer dd = hh;
+
+//        3. 通过Byte获取byte的最大值
+
+        Byte kk = Byte.MAX_VALUE;
+        System.out.println(kk);
+
+    }
+}
---
+++ src/com/how2java/zhuangxiang/train/train2.java
@@ -0,0 +1,30 @@
+package com.how2java.zhuangxiang.train;
+
+/**
+ * Created by LuYaoSheng
+ * Created time 2019/02/19 22:33
+ * Description
+ */
+public class train2 {
+    public static void main(String[] args) {
+
+        //int的最大值
+        System.out.println(Integer.MAX_VALUE);
+        //int的最小值
+        System.out.println(Integer.MIN_VALUE);
+
+//        把浮点数 3.14 转换为 字符串 "3.14"
+        Double pai = 3.14;
+        String a = String.valueOf(pai);
+        System.out.println("1.  " + a);
+
+//        再把字符串 “3.14” 转换为 浮点数 3.14
+        pai = Double.valueOf(a);
+        System.out.println("2.  " + pai);
+//        如果字符串是 3.1a4，转换为浮点数会得到什么？
+        String asd = "3.1a4";
+        Double aa = Double.valueOf(asd);
+//        System.out.println("3.  " + aa);  抛出异常
+
+    }
+}
